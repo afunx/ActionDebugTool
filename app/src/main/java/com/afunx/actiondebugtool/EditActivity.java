57739ecd0d9c2570dc7a8c7e78fd9232d7651454
白桦 <afunx.bai@ubtrobot.com>
2018-01-12 09:45:31 +0800

    remove rubbish import in EditActivity.java

--- app/src/main/java/com/afunx/actiondebugtool/EditActivity.java
+++ app/src/main/java/com/afunx/actiondebugtool/EditActivity.java
@@ -3,13 +3,11 @@ package com.afunx.actiondebugtool;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
-import android.widget.SeekBar;
 import android.widget.Toast;
 
 public class EditActivity extends AppCompatActivity implements View.OnClickListener {
