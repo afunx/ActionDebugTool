b000059d74484af837bffd7d63d3c97d97319479
白桦 <afunx.bai@ubtrobot.com>
2018-01-17 10:43:07 +0800

    Constants.java is added into common

---
+++ app/src/main/java/com/afunx/actiondebugtool/common/Constants.java
@@ -0,0 +1,20 @@
+package com.afunx.actiondebugtool.common;
+
+/**
+ * Created by afunx on 16/01/2018.
+ */
+
+public interface Constants {
+
+    int MOTORS_COUNT = 14;
+
+    int[] MOTORS_DEG_MIN = new int[]{-120, -50, -120, -50, -120, -120, -50, -120, -120, -50, -10, -12, -50, -60};
+
+    int[] MOTORS_DEG_MAX = new int[]{120, 50, 120, 50, 120, 120, 12, 120, 120, 12, 10, 10, 50, 100};
+
+    int RUNTIME_MIN = 100;
+
+    int RUNTIME_MAX = 5000;
+
+    int RUNTIME_DEFAULT = 1000;
+}
